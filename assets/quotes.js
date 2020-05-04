84ba1693a55227d251c519a2e31601e144e93529
zaenal123404 <64802129+zaenal123404@users.noreply.github.com>
2020-05-05 00:49:38 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "anonim", quotes: "Whatever happens, keep breathing" },
+    { author: "anonim", quotes: "Semangat Yah " },
+    { author: "anonim", quotes: "Hey! I luv yu" },
+    { author: "anonim", quotes: "I love you." },
+    { author: "anonim", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "anonim", quotes: "I love you." },
+    { author: "anonim", quotes: "I'm sorry for loving you." },
 ];
